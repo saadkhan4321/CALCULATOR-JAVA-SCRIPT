7fb952380051584d0ab5de51eb17e1bf624d22a8
saadkhan4321 <117026264+saadkhan4321@users.noreply.github.com>
2023-01-24 14:36:05 +0500

    Add files via upload

---
+++ loop;/index1.js
@@ -0,0 +1,15 @@
+function add(num1,num2){
+    document.write(num1+num2)
+}
+function sub(num1,num2){
+    document.write(num1-num2)
+}
+function mult(num1,num2){
+    document.write(num1*num2)
+}
+function divide(num1,num2){
+    document.write(num1/num2)
+}
+function reminder(num1,num2){
+    document.write(num1%num2)
+}
